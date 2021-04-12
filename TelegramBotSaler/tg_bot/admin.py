aa1c004546efd398aad974312cf297850fd54ad1
Oleg <oleg20064@gmail.com>
2021-04-12 22:06:25 +0300

    Оптимизация бота. Изменение админки. Оптимизировал обработчики сценария. Теперь наткнуться на ошибку сложнее.

--- TelegramBotSaler/tg_bot/admin.py
+++ TelegramBotSaler/tg_bot/admin.py
@@ -1,6 +1,9 @@
+# from admin_interface.admin import ThemeAdmin
+# from admin_interface.models import Theme
 from django.contrib import admin
 
 # Register your models here.
+from tg_bot.models import TokenSale, Users, Themes, QuestionSuggestions, ReminderUsers
 
 
 class TokenAdmin(admin.ModelAdmin):
@@ -21,8 +24,8 @@ class UsersAdmin(admin.ModelAdmin):
     def mark_as_user(self, request, queryset):
         queryset.update(is_moderator=False)
 
-    mark_as_moderator.short_description = 'Перевести статус модератора'
-    mark_as_user.short_description = 'Перевести статус пользователя'
+    mark_as_moderator.short_description = 'Перевести в статус модератора'
+    mark_as_user.short_description = 'Перевести в статус пользователя'
 
 
 class ThemesAdmin(admin.ModelAdmin):
@@ -37,3 +40,11 @@ class QuestionAdmin(admin.ModelAdmin):
 class ReminderUsersAdmin(admin.ModelAdmin):
     list_display = ['user', 'token_sale', 'twelve', 'one']
     list_filter = ['user']
+
+
+# admin.site.register(TokenSale, TokenAdmin)
+# admin.site.register(Users, UsersAdmin)
+# admin.site.register(Themes, ThemesAdmin)
+# admin.site.register(QuestionSuggestions, QuestionAdmin)
+# admin.site.register(ReminderUsers, ReminderUsersAdmin)
+# admin.site.register(Theme, ThemeAdmin)
